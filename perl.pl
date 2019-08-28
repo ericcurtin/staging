42c409aaf29fa1609e008d0df2dcb8e98bdfe601
Eric Curtin <ericcurtin17@gmail.com>
2019-08-28 21:12:40 +0100

    Use inotify, not polling

--- perl.pl
+++ perl.pl
@@ -6,19 +6,12 @@ use warnings;
 my $file = 'a.txt';
 open my $info, $file or die "Could not open $file: $!";
 
-my $i = 0;
-my $j = 0;
 while (1) {
 while (my $line = <$info>)  {   
     print "$line\n";
-    ++$i;
 }
-  if ($i > $j) {
   print "complete\n";
-  $j = $i;
-  }
-
-  select(undef, undef, undef, .01);
+  qx(inotifywait a.txt);
 }
 
 close $info;
