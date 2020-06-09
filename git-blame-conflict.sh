1badeaae703a899a7e3457412c21b889a653d0dd
Eric Curtin <eric.curtin@dell.com>
2020-06-09 17:37:22 +0100

    Multi-process and cut the amount of total git blames is half
    Will git blame upto 16 files at once

--- git-blame-conflict.sh
+++ git-blame-conflict.sh
@@ -2,10 +2,41 @@
 
 ls_files=$(git diff --name-only --diff-filter=U)
 
-printf "$ls_files" | xargs -I{} git blame {} | awk '/<<<</,/>>>>/' | cut -c-32 | grep -o '..................$' | grep -v "Not Committed Yet" | sort | uniq -c | sort -nr | head -n 8
+out=/tmp/git-blame-conflict-$$.lock
 
-for i in $(printf "%s\n" $ls_files); do
-  printf "\n%s\n" $i
-  git blame $i | awk '/<<<</,/>>>>/' | cut -c-32 | grep -o '..................$' | grep -v "Not Committed Yet" | sort | uniq -c | sort -nr | head -n 4 
+blame_res_cnt_func() {
+  if [ -n "$blame_res" ]; then
+    echo "$blame_res" | sort | uniq -c | sort -nr | head -n4
+  else
+    echo "        File doesn't exist on one side of branch"
+  fi
+}
+
+task() {
+  blame_res=$(git blame $i | awk '/<<<</,/>>>>/' | cut -c-32 | grep -o '..................$' | grep -v "Not Committed Yet")
+  blame_res_cnt=$(blame_res_cnt_func)
+
+  {
+    flock -x 3
+    printf "%s\n%s\n\n" "$i" "$blame_res_cnt"
+    echo  "$blame_res" >&3
+  } 3>>$out
+}
+
+touch $out
+
+for i in $(echo $ls_files); do
+  # Max 16 processes at any given time
+  while [[ $(jobs -r -p | wc -l) -gt 16 ]]; do
+    sleep 1
+  done
+
+  task &
 done
+
+wait
+
+echo "Total amount of lines changed per person: "
+cat $out | sort | uniq -c | sort -nr | head -n8
+rm $out
 
