67efb4ac7bbd87070fc96915bf73adf771dcbda6
Eric Curtin <ecurtin@redhat.com>
2021-10-15 14:56:37 +0100

    An extension to fedora-arm-image-installer
    That also flashes firmware, answers prompts. Parameters are:
    img_name dev_name fw.zip

---
+++ img2rpi.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+
+prompt_and_read() {
+  if [ -z "$2" ]; then
+    echo
+    sudo fdisk -l | grep -A2 "/dev/m\|/dev/s" | sed "s/://g"
+    echo
+    read -p "Disk to write to: " disk
+    echo
+    echo "Writing $1 to $disk"
+    echo
+  else
+    disk=$2
+  fi
+}
+
+sudo sync
+
+if false; then
+if [[ $1 == *.raw.xz ]]; then
+#  xzcat $1 | sudo dd bs=4M iflag=fullblock oflag=direct status=progress of=/dev/mmcblk0
+  prompt_and_read $1 $2
+  echo "yes" | sudo fedora-arm-image-installer --image=$1 --media=$disk --addkey=$HOME/.ssh/id_rsa.pub --resizefs --showboot --target=rpi4 -y
+  echo "Completed write to $disk"
+elif [[ $1 == *.iso ]]; then
+  prompt_and_read $1 $2
+  sudo dd of=$disk if=$1 bs=4M conv=nocreat,notrunc status=progress
+  echo "Completed write to $disk"
+else
+  echo "Unrecognized extension in filename $1"
+fi
+fi
+
+fw_file=$(realpath $3)
+if [[ $3 == *.zip ]]; then
+  dev="$(sudo fdisk -l | grep "$disk" | grep FAT | awk '{print $1}')"
+  sudo mkdir -p /tmp$dev
+  sudo mount $dev /tmp$dev
+  cd /tmp$dev
+  sudo unzip -o $fw_file
+  cd -
+  sudo umount /tmp$dev
+  sudo rm -rf /tmp$dev
+else
+  echo "Unrecognized extension in filename $3"
+fi
+
+sudo sync
+
