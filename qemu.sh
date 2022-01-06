480d830f0ae0f39975f00da6620692c4011404b9
Eric Curtin <ecurtin@redhat.com>
2022-01-06 12:14:23 +0000

    Change networking
    Camera still not working

--- qemu.sh
+++ qemu.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-dir="/opt/homebrew/bin/../share/qemu/"
+# -device virtio-net-pci,netdev=net \
+# -netdev user,id=net,ipv6=off \
 
 qemu-system-aarch64 \
          -machine virt,accel=hvf,highmem=off \
@@ -11,13 +12,12 @@ qemu-system-aarch64 \
          -device qemu-xhci \
          -device virtio-gpu-gl-pci \
          -device usb-kbd \
-         -device virtio-net-pci,netdev=net \
          -device virtio-mouse-pci \
          -display cocoa,gl=es \
-         -netdev user,id=net,ipv6=off \
+         -netdev vmnet-shared,id=n1 \
+         -device virtio-net,netdev=n1 \
          -drive "if=pflash,format=raw,file=./edk2-aarch64-code.fd,readonly=on" \
          -drive "if=pflash,format=raw,file=./edk2-arm-vars.fd,discard=on" \
          -drive "if=virtio,format=raw,file=./hdd.raw,discard=on" \
-         -full-screen \
          -boot d
 
