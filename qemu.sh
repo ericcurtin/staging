d8284e2a5b95f095292f7acecab7e0a5c9dfa714
Eric Curtin <ecurtin@redhat.com>
2022-01-08 19:09:06 +0000

    Add x86 vm with usb passthrough

--- qemu.sh
+++ qemu.sh
@@ -28,7 +28,19 @@ if false; then # graphics version
     -drive "$fw_opts,file=$dir/edk2-aarch64-code.fd,readonly=on" \
     -drive "$fw_opts,file=$dir/edk2-arm-vars.fd,discard=on" \
     -drive "if=virtio,format=raw,file=$dir/hdd.raw,discard=on"
-elif false; then
+elif [ $(uname -m) = "x86_64" ]; then
+  /usr/bin/qemu-system-x86_64 \
+    -machine pc-q35-6.1,accel=kvm -cpu host -m 6G -smp 12 \
+    -chardev socket,path=/tmp/port1,server=on,wait=off,id=port1-char \
+    -device virtio-serial \
+    -device virtserialport,id=port1,chardev=port1-char,name=org.fedoraproject.port.0 \
+    -net user,hostfwd=tcp::8022-:22 \
+    -drive if=virtio,file=/var/lib/libvirt/images/fedora35.qcow2,format=qcow2 \
+    -usb -device usb-ehci,id=ehci -device usb-host,hostbus=1,hostaddr=2 \
+    -net nic -display gtk
+
+    # -usb -device usb-ehci,id=ehci -device usb-host,hostbus=1,hostaddr=2 for cam
+else
   /opt/homebrew/bin/qemu-system-aarch64 \
     -m 6G -smp 8 \
     -chardev socket,path=/tmp/port1,server=on,wait=off,id=port1-char \
@@ -41,14 +53,5 @@ elif false; then
     -drive file=$dir/edk2-aarch64-code.fd,$fw_opts,readonly=on \
     -drive file=$dir/edk2-arm-vars.fd,$fw_opts \
     -drive if=virtio,file=$dir/hdd.raw,format=raw -display cocoa
-else
-  /usr/bin/qemu-system-x86_64 \
-    -machine pc-q35-6.1,accel=kvm -cpu Cooperlake -m 6G -smp 12 \
-    -chardev socket,path=/tmp/port1,server=on,wait=off,id=port1-char \
-    -device virtio-serial \
-    -device virtserialport,id=port1,chardev=port1-char,name=org.fedoraproject.port.0 \
-    -net user,hostfwd=tcp::8022-:22 \
-    -drive if=virtio,file=/var/lib/libvirt/images/fedora35.qcow2,format=qcow2 \
-    -net nic -display gtk
 fi
 
