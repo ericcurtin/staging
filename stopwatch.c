9b9c88f85388c769fdf12b4dca688fa9fb902df8
Eric Curtin <ecurtin@redhat.com>
2022-01-09 09:54:55 +0000

    less division

--- stopwatch.c
+++ stopwatch.c
@@ -6,15 +6,15 @@ int main() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
   unsigned long long msSinceEpoch =
-    (unsigned long long)(tv.tv_sec) * 1000 +
-    (unsigned long long)(tv.tv_usec) / 1000;
+    (unsigned long long)(tv.tv_sec) * 1000000 +
+    (unsigned long long)(tv.tv_usec);
 
   while (1) {
     gettimeofday(&tv, NULL);
     unsigned long long msSinceEpoch1 =
-      (unsigned long long)(tv.tv_sec) * 1000 +
-      (unsigned long long)(tv.tv_usec) / 1000;
-    printf("\r%.3f", (msSinceEpoch1 - msSinceEpoch) / 1000.0);
+      (unsigned long long)(tv.tv_sec) * 1000000 +
+      (unsigned long long)(tv.tv_usec);
+    printf("\r%.2f", (msSinceEpoch1 - msSinceEpoch) / 1000000.0);
     fflush(stdout);
     usleep(100000);
   }
