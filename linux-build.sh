878d06da0c06fccf628532b65986c7b4bd627cf0
Eric Curtin <ecurtin@redhat.com>
2022-02-28 15:57:32 +0000

    changes

--- linux-build.sh
+++ linux-build.sh
@@ -10,10 +10,9 @@ make -j$j
 make modules_install -j$j
 make dtbs_install
 make install
-for old in /boot/dtbs/*; do
-  new="$(echo $old | sed 's#dtbs/#dtb-#g')"
-  mv $old $new
-done
+cd /boot
+to_link="$(ls -tr | grep dtb | grep -v ^dtb$ | head -n1)"
+ln -sf $to_link dtb
 
 # mv /boot/dtbs/<new-version> /boot/dtb-<new-version>
 # grubby --set-default /boot/<vmlinuz-new-version>
--- ssh-list.sh
+++ ssh-list.sh
@@ -3,7 +3,7 @@
 set -e
 
 for i in 192.168.1.0 10.42.0.220 10.42.0.220; do
-  sudo timeout 8 nmap --host-timeout 1s -sS -p 22 $i/24 &
+  sudo timeout 16 nmap --host-timeout 1s -sS -p 22 $i/24 &
 done | grep report
 
 wait
