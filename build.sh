4f5d3f808712f02780f4d65288aafdee75de4fc1
Eric Curtin <ecurtin@redhat.com>
2022-03-08 18:19:22 +0000

    Linux kernel

--- build.sh
+++ build.sh
@@ -9,7 +9,9 @@ path="~/git/$base/"
 if [ -f "meson.build" ]; then
   cmd="meson build --prefix=/usr && ninja -v -C build && sudo ninja -v -C build install"
 elif [ -f "autogen.sh" ]; then
-  cmd="./autogen.sh && make -j$(nproc) && sudo make install"
+  cmd="./autogen.sh && make -j\$(nproc) && sudo make install"
+elif [ -f "Makefile" ] && [ -f "Kbuild" ] && [ -f "Kconfig" ]; then # linux kern
+  cmd="if [ ! -f '.config' ]; then cp /boot/config-\$(uname -r) .; fi && make olddefconfig && make -j\$(nproc)"
 fi
 
 if [ -z "$host" ]; then
