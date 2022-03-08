7743f6a679c65fed5f2c9bbdc7b675ac39653a85
Eric Curtin <ecurtin@redhat.com>
2022-03-08 14:37:45 +0000

    generic build script which detects build system

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+host="$1"
+
+base="$(basename $PWD)"
+path="~/git/$base/"
+if [ -f "meson.build" ]; then
+  cmd="meson build --prefix=/usr && ninja -v -C build && sudo ninja -v -C build install"
+elif [ -f "autogen.sh" ]; then
+  cmd="./autogen.sh && make -j$(nproc) && sudo make install"
+fi
+
+if [ -z "$host" ]; then
+  /bin/bash -c "$cmd"
+else
+  clean="&& rm -rf build"
+  if [ -z "$2" ]; then
+    unset clean
+  fi
+
+  ssh $host "cd $path $clean && $cmd"
+fi
+
