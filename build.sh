8382557dcff037ab307e56e4f921817756c934e0
Eric Curtin <ecurtin@redhat.com>
2022-03-12 23:14:11 +0000

    Prefer cmake over autotools

--- build.sh
+++ build.sh
@@ -8,14 +8,14 @@ base="$(echo $PWD | sed "s#$HOME##g")"
 path="~/$base/"
 if [ -f "meson.build" ]; then
   cmd="meson build --prefix=/usr && ninja -v -C build && sudo ninja -v -C build install"
-elif [ -f "autogen.sh" ]; then
+elif [ -f "CMakeLists.txt" ]; then # sdl prefer this over autogen.sh
   if [ -f "sdl2.m4" ]; then # sdl specific
-    extra="--enable-video-kmsdrm"
+    extra="-DSDL_VIDEO_KMSDRM=ON"
   fi
 
+  cmd="mkdir -p build && cd build && cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=Release $extra .. && make -j\$(nproc) VERBOSE=1 && sudo make install"
+elif [ -f "autogen.sh" ]; then
   cmd="./autogen.sh --prefix=/usr && if [ -f 'configure' ]; then ./configure --prefix=/usr $extra; fi && make -j\$(nproc) && sudo make install"
-elif [ -f "CMakeLists.txt" ]; then
-  cmd="mkdir -p build && cd build && cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=Release .. && make -j\$(nproc) VERBOSE=1 && sudo make install"
 elif [ -f "Cargo.lock" ]; then
   cmd="sudo cargo install --path ."
 elif [ -f "Makefile" ] && [ -f "Kbuild" ] && [ -f "Kconfig" ]; then # linux kern
