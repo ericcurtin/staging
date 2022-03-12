c5b3260e4bde06dfbfda539391e20c6c12923fc0
Eric Curtin <ecurtin@redhat.com>
2022-03-12 23:06:29 +0000

    sdl stuff

--- build.sh
+++ build.sh
@@ -9,13 +9,19 @@ path="~/$base/"
 if [ -f "meson.build" ]; then
   cmd="meson build --prefix=/usr && ninja -v -C build && sudo ninja -v -C build install"
 elif [ -f "autogen.sh" ]; then
-  cmd="./autogen.sh --prefix=/usr && if [ -f 'configure' ]; then ./configure --prefix=/usr; fi && make -j\$(nproc) && sudo make install"
+  if [ -f "sdl2.m4" ]; then # sdl specific
+    extra="--enable-video-kmsdrm"
+  fi
+
+  cmd="./autogen.sh --prefix=/usr && if [ -f 'configure' ]; then ./configure --prefix=/usr $extra; fi && make -j\$(nproc) && sudo make install"
 elif [ -f "CMakeLists.txt" ]; then
   cmd="mkdir -p build && cd build && cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=Release .. && make -j\$(nproc) VERBOSE=1 && sudo make install"
 elif [ -f "Cargo.lock" ]; then
   cmd="sudo cargo install --path ."
 elif [ -f "Makefile" ] && [ -f "Kbuild" ] && [ -f "Kconfig" ]; then # linux kern
   cmd="if [ ! -f '.config' ]; then cp /boot/config-\$(uname -r) .; fi && make olddefconfig && make -j\$(nproc)"
+elif [ -f "Makefile" ]; then
+  cmd="make -j\$(nproc)"
 fi
 
 if [ -z "$host" ]; then
