f5908320246c146b2dda99eef901dce3560676c2
Eric Curtin <ecurtin@redhat.com>
2022-03-12 15:55:34 +0000

    build.sh add cmake

--- .bashrc
+++ .bashrc
@@ -5,7 +5,7 @@ if [ -f /etc/bashrc ]; then
 	. /etc/bashrc
 fi
 
-export PATH="$HOME/go/bin:$HOME/git/depot_tools:$HOME/git/staging:$HOME/git/dns:/var/lib/snapd/snap/bin:$HOME/.local/bin:$HOME/bin:$PATH"
+export PATH="$HOME/go/bin:$HOME/git/staging:$HOME/git/dns:/var/lib/snapd/snap/bin:$HOME/.local/bin:$HOME/bin:$PATH"
 
 # Uncomment the following line if you don't like systemctl's auto-paging feature:
 # export SYSTEMD_PAGER=
--- build.sh
+++ build.sh
@@ -10,6 +10,8 @@ if [ -f "meson.build" ]; then
   cmd="meson build --prefix=/usr && ninja -v -C build && sudo ninja -v -C build install"
 elif [ -f "autogen.sh" ]; then
   cmd="./autogen.sh --prefix=/usr && make -j\$(nproc) && sudo make install"
+elif [ -f "CMakeLists.txt" ]; then
+  cmd="mkdir -p build && cd build && cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=Release .. && make -j\$(nproc) VERBOSE=1 && sudo make install"
 elif [ -f "Makefile" ] && [ -f "Kbuild" ] && [ -f "Kconfig" ]; then # linux kern
   cmd="if [ ! -f '.config' ]; then cp /boot/config-\$(uname -r) .; fi && make olddefconfig && make -j\$(nproc)"
 fi
