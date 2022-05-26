c93a77f3917e3cbf3a87c538174f24d703f97a2b
Eric Curtin <ecurtin@redhat.com>
2022-05-26 10:31:36 +0100

    Add cam start

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ if [ -f "meson.build" ]; then
   # -Db_sanitize=address
   # --buildtype=debug
   # --buildtype=release
-  cmd="meson build --buildtype=debug --prefix=/usr && ninja -v -C build && sudo ninja -v -C build install"
+  cmd="meson build --buildtype=release --prefix=/usr && ninja -v -C build && sudo ninja -v -C build install"
 elif [ -f "CMakeLists.txt" ]; then # sdl prefer this over autogen.sh
   if [ -f "sdl2.m4" ]; then # sdl specific
     extra="-DSDL_DLOPEN=ON \
@@ -82,9 +82,9 @@ elif [ -d "osbuild-manifests" ]; then
   cmd="cd osbuild-manifests; sudo make -j\$(nproc) cs9-rpi4-developer-direct.aarch64.img"
 fi
 
-# export CFLAGS="-O1 -ggdb"; export CXXFLAGS="$CFLAGS"; export LDFLAGS="$CFLAGS";
+export CFLAGS="-O0 -ggdb"; export CXXFLAGS="$CFLAGS"; export LDFLAGS="$CFLAGS";
 #export CFLAGS="$CFLAGS -fsanitize=address"; export CXXFLAGS="$CFLAGS"; export LDFLAGS="$CFLAGS"
-#cmd="export CFLAGS='$CFLAGS'; export CXXFLAGS='$CFLAGS'; export LDFLAGS='$CFLAGS'; $cmd"
+cmd="export CFLAGS='$CFLAGS'; export CXXFLAGS='$CFLAGS'; export LDFLAGS='$CFLAGS'; $cmd"
 # cmd="if command -v ccache > /dev/null; then export CC='ccache gcc'; export CXX='ccache g++'; fi && $cmd"
 # cmd="export CC='gcc'; export CXX='g++'; $cmd"
 # cmd="export CC='clang'; export CXX='clang++'; $cmd"
---
+++ cam-start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+cam_num=$(cam -l 2>&1 | grep -i logitech | awk -F':' '{printf $1"\n"}')
+
+cam -c$cam_num -S -C0
+
