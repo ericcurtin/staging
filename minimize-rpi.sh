7c74d0696395abd86cdd3932811ea1e58f5d7169
Eric Curtin <ecurtin@redhat.com>
2022-07-07 14:28:26 +0100

    Time boots

--- minimize-rpi.sh
+++ minimize-rpi.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+echo "initial boot time"
+ssh guest@10.42.0.220 "sudo systemd-analyze && sudo du -sh /boot/initramfs* && sudo reboot"
+echo "rebooting and sleeping for 64 seconds"
+echo
+sleep 64
+
+echo "initial boot time 2nd boot"
+ssh guest@10.42.0.220 "sudo systemd-analyze && sudo du -sh /boot/initramfs* && sudo dracut -f && sudo reboot"
+echo "rebooting and sleeping for 64 seconds"
+echo
+sleep 64
+
 for i in $(ssh guest@10.42.0.220 "ls /usr/lib/dracut/modules.d/"); do
-  if [ "$i" == "00systemd" ] || [ "$i" == "01systemd-initrd" ] || [ "$i" == "05nss-softokn" ]; then
-    echo "skipping '$i'"
+  if [ "$i" == "00systemd" ] || [ "$i" == "01systemd-initrd" ] || [ "$i" == "05nss-softokn" ] || [ "$i" == "90dmraid" ]; then
+    echo "'$i' skip"
+    echo
     continue
   fi
 
-  echo "boot-time, sizeof before removal and removing $i"
+  echo "'$i' removal, boot-time, sizeof before removal"
   ssh guest@10.42.0.220 "sudo systemd-analyze && sudo du -sh /boot/initramfs* && sudo mv /usr/lib/dracut/modules.d/$i /root/modules.d/ && sudo dracut -f && sudo reboot"
   echo "rebooting and sleeping for 64 seconds"
   echo
