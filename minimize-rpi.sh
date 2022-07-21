90171cf7bc0e2bf8dfbf245c67907411cbeee249
Eric Curtin <ecurtin@redhat.com>
2022-07-21 14:18:46 +0100

    changes

--- minimize-rpi.sh
+++ minimize-rpi.sh
@@ -4,8 +4,13 @@ set -e
 
 host="192.168.1.136"
 
+ssh guest@$host "sudo dnf install -y 'dnf-command(config-manager)'"
+ssh guest@$host "sudo dnf config-manager --set-enabled crb"
+ssh guest@$host "sudo dnf install -y epel-release"
 ssh guest@$host "sudo dnf install -y git gcc g++ libevent libevent-devel \
-  openssl openssl-devel gnutls gnutls-devel meson boost boost-devel python3-pip"
+  openssl openssl-devel gnutls gnutls-devel meson boost boost-devel \
+  python3-pip systemd-udev doxygen cmake graphviz libatomic texlive-latex"
+ssh guest@$host "sudo pip install jinja2 ply pyyaml"
 
 cd ../libcamera
 git-push.sh guest@$host
@@ -15,16 +20,19 @@ cd ../twincam
 git-push.sh guest@$host
 cd -
 
-ssh guest@$host 
+ssh guest@$host "cd git/libcamera && meson build --prefix=/usr && ninja -v -C build && sudo ninja -v -C build install"
+ssh guest@$host "cd git/twincam && meson build --prefix=/usr && ninja -v -C build && sudo ninja -v -C build install && ./install-minimal.sh"
 
 echo "initial boot time"
-ssh guest@$host "sudo mkdir -p /root/modules && sudo systemd-analyze && sudo du -sh /boot/initramfs* && sudo reboot" || true
+perf="sudo systemd-analyze && sudo du -sh /boot/initramfs* &&"
+ssh guest@$host "sudo mkdir -p /root/modules && $perf sudo reboot" || true
 echo "rebooting and sleeping for 64 seconds"
 echo
 sleep 64
 
+perf="sudo journalctl -r | grep 'seq: 0' | head -n1 && $perf"
 echo "initial boot time 2nd boot"
-ssh guest@$host "sudo systemd-analyze && sudo du -sh /boot/initramfs* && sudo dracut -f && sudo reboot" || true
+ssh guest@$host "$perf sudo dracut -f && sudo reboot" || true
 echo "rebooting and sleeping for 64 seconds"
 echo
 sleep 64
@@ -50,7 +58,7 @@ for i in $(ssh guest@$host "ls /usr/lib/dracut/modules.d/"); do
   fi
 
   if [ "$tested" == "false" ]; then
-    pre="sudo systemd-analyze && sudo du -sh /boot/initramfs* &&"
+    pre="$perf"
     post="&& sudo dracut -f && sudo reboot"
   fi
 
@@ -64,7 +72,7 @@ for i in $(ssh guest@$host "ls /usr/lib/dracut/modules.d/"); do
 done
 
 tested="false"
-pre="sudo systemd-analyze && sudo du -sh /boot/initramfs* &&"
+pre="$perf"
 post="&& sudo dracut -f && sudo reboot"
 ssh guest@$host "$pre true $post" || true
 if [ "$tested" == "false" ]; then
@@ -74,7 +82,7 @@ if [ "$tested" == "false" ]; then
 fi
 
 for i in $(ssh guest@$host "sudo lsinitrd -s | grep -i lib/modules | tac | awk '{print \$NF}' | grep -v ext4"); do
-  if [ "$i" == "crc32c" ]; then
+  if [ "$i" == */modules.alias.bin ]; then
     echo "skipping: '$i'"
     continue
   fi
