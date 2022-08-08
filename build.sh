4f560b8c459d138d01726a6c4080086075a76f80
Eric Curtin <ecurtin@redhat.com>
2022-08-08 15:21:33 +0100

    changes

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 host="$1"
-
+njobs=6
 base="$(echo $PWD | sed "s#$HOME##g")"
 path="~/$base/"
 if [ -f "meson.build" ]; then
@@ -15,7 +15,7 @@ elif [ -f "SDL_image.h" ]; then
   extra="--disable-dependency-tracking --disable-jpg-shared \
          --disable-png-shared --disable-tif-shared --disable-webp-shared \
          --disable-static"
-  cmd="if [ ! -f 'Makefile' ]; then ./autogen.sh && if [ -f 'configure' ]; then ./configure --prefix=/usr $extra; sed -i -e 's! -shared ! -Wl,--as-needed\0!g' libtool; fi; fi && make -j\$(nproc) V=1 && sudo make install"
+  cmd="if [ ! -f 'Makefile' ]; then ./autogen.sh && if [ -f 'configure' ]; then ./configure --prefix=/usr $extra; sed -i -e 's! -shared ! -Wl,--as-needed\0!g' libtool; fi; fi && make -j$njobs V=1 && sudo make install"
 elif [ -f "CMakeLists.txt" ]; then # sdl prefer this over autogen.sh
   if [ -f "sdl2.m4" ]; then # sdl specific
     extra="-DSDL_DLOPEN=ON \
@@ -59,7 +59,7 @@ fi
 
   # -DCMAKE_BUILD_TYPE=Release
   # -DCMAKE_BUILD_TYPE=Debug this meses up the ln -s
-  cmd="mkdir -p build && cd build && cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=Release $extra .. && make -j\$(nproc) VERBOSE=1 && sudo make install $extra2"
+  cmd="mkdir -p build && cd build && cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=Release $extra .. && make -j$njobs VERBOSE=1 && sudo make install $extra2"
 elif [ -f "autogen.sh" ]; then
   if grep -q plymouth README; then
     extra="--enable-tracing                                      \
@@ -75,7 +75,7 @@ elif [ -f "autogen.sh" ]; then
   fi
 
   # inotify-tools didn't like autogen.sh --prefix=/usr
-  cmd="if [ ! -f 'Makefile' ]; then ./autogen.sh && if [ -f 'configure' ]; then ./configure --prefix=/usr $extra; fi; fi && make -j\$(nproc) V=1 && sudo make install"
+  cmd="if [ ! -f 'Makefile' ]; then ./autogen.sh && if [ -f 'configure' ]; then ./configure --prefix=/usr $extra; fi; fi && make -j$njobs V=1 && sudo make install"
 elif [ -f "Cargo.lock" ]; then
   cmd="sudo cargo install --path ."
 elif [ -f "Cargo.lock" ]; then
@@ -83,7 +83,7 @@ elif [ -f "Cargo.lock" ]; then
 elif [ -f "Makefile" ] && [ -f "Kbuild" ] && [ -f "Kconfig" ]; then
   # dnf -y install ncurses-devel flex bison elfutils-libelf-devel dwarves ccache zstd bc rpm-build bpftool gcc perl-devel perl-generators python3-devel elfutils-devel the_silver_searcher openssl-devel openssl system-sb-certs dracut linux-firmware binutils-devel gcc-plugin-devel glibc-static libcap-devel libcap-ng-devel libmnl-devel llvm nss-tools numactl-devel pesign python3-docutils libbpf-devel
   if [ -e "/boot/dtb" ]; then
-    cmd="if [ ! -f '.config' ]; then cp /boot/config-\$(uname -r) .; fi && make olddefconfig && make -j\$(nproc) && make -j\$(nproc) modules && sudo make modules_install && sudo make dtbs_install && sudo make install"
+    cmd="if [ ! -f '.config' ]; then cp /boot/config-\$(uname -r) .; fi && make olddefconfig && make -j$njobs && make -j$njobs modules && sudo make modules_install && sudo make dtbs_install && sudo make install"
 #    cd /boot
 #    to_link=$(for i in $(ls -t | grep dtb); do if [ -d "$i" ]; then echo $i; break; fi; done)
 #    sudo ln -sf $to_link dtb
@@ -94,9 +94,9 @@ elif [ -f "Makefile" ] && [ -f "Kbuild" ] && [ -f "Kconfig" ]; then
     cmd="if [ ! -f '.config' ]; then cp /boot/config-\$(uname -r) .; fi && make olddefconfig && make -j8 && make -j8 bzImage && make -j8 modules && sudo make modules_install && sudo make install"
   fi
 elif [ -f "Makefile" ]; then
-  cmd="make -j\$(nproc)"
+  cmd="make -j$njobs"
 elif [ -d "osbuild-manifests" ]; then
-  cmd="cd osbuild-manifests; sudo make -j\$(nproc) cs9-rpi4-developer-direct.aarch64.img"
+  cmd="cd osbuild-manifests; sudo make -j$njobs cs9-rpi4-developer-direct.aarch64.img"
 fi
 
 # export CFLAGS="-O0 -ggdb"; export CXXFLAGS="$CFLAGS"; export LDFLAGS="$CFLAGS";
