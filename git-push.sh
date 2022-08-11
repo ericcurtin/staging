d06cdbc437e08a561d685d2a6538d898a806040e
Eric Curtin <ecurtin@redhat.com>
2022-08-11 10:26:04 +0100

    port option

--- git-push.sh
+++ git-push.sh
@@ -2,9 +2,36 @@
 
 set -e
 
+
+help()
+{
+   echo "Add description of the script functions here."
+   echo
+   echo "Syntax: scriptTemplate [-h|p]"
+   echo "options:"
+   echo "-h    print this help"
+   echo "-p    specify non-22 port"
+   echo
+}
+
+port="22"
+while getopts ":hp:" option; do
+   case $option in
+      h) # display Help
+         help
+         exit;;
+      p) # Enter a name
+         port=$OPTARG;;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+shift $(($OPTIND - 1))
 host="$1"
 
-export GIT_SSH_COMMAND='ssh'
+export GIT_SSH_COMMAND="ssh -p$port"
 branch="$(git rev-parse --abbrev-ref HEAD)"
 base="$(basename $PWD)"
 path="~/git/$base/"
