0d1fb321747367189d6802d315b042d1778df147
Eric Curtin <ericcurtin17@gmail.com>
2022-10-05 16:06:22 +0100

    Rebuild ostree

--- build-aboot-ostree.sh
+++ build-aboot-ostree.sh
@@ -16,10 +16,21 @@ task2() {
   rpmbuild -bs autosig-qemu-dtb.spec
 }
 
+task3() {
+  cd ~/git/ostree/
+  rm -rf *.tar.xz
+  tar -cJf libostree-2022.5.tar.xz *
+  cd -
+  cp ~/git/ostree/* /home/ecurtin/rpmbuild/SOURCES/
+  sudo rm -rf /home/ecurtin/rpmbuild/SRPMS/ostree*.src.rpm
+  rpmbuild -bs ostree.spec
+}
+
 sudo rm -rf /var/lib/mock/centos-stream+epel-9-aarch64/result
 cd /home/ecurtin/rpmbuild/SOURCES/
 task1 &
 task2 &
+task3 &
 wait
 sudo mock -r centos-stream+epel-9-aarch64 --rebuild /home/ecurtin/rpmbuild/SRPMS/aboot-update-0.1-2.fc36.src.rpm
 sudo mock -a https://buildlogs.centos.org/9-stream/automotive/aarch64/packages-main/ -a https://buildlogs.centos.org/9-stream/autosd/aarch64/packages-main/ -r centos-stream+epel-9-aarch64 --rebuild /home/ecurtin/rpmbuild/SRPMS/autosig-qemu-dtb-0.1-2.fc36.src.rpm
