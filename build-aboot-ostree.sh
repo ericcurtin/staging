e1e9f52145f56c45ca40a57ce8326e78e7454c7d
Eric Curtin <ecurtin@redhat.com>
2022-10-13 13:14:35 +0100

    Better name for tasks

--- build-aboot-ostree.sh
+++ build-aboot-ostree.sh
@@ -4,21 +4,21 @@ set -ex
 
 nohup sudo dnf clean all &
 
-task1() {
+build-aboot-update() {
   cp ~/git/aboot-update/* /home/ecurtin/rpmbuild/SOURCES/
   sudo rm -rf /home/ecurtin/rpmbuild/SRPMS/aboot-update-0.1-2.fc36.src.rpm
   rpmbuild -bs aboot-update.spec
   sudo podman run --rm --privileged -v /home/ecurtin/rpmbuild/:/home/ecurtin/rpmbuild/ -ti conmock /bin/bash -c "mock -r centos-stream+epel-9-aarch64 --rebuild --resultdir /var/lib/mock/centos-stream+epel-9-aarch64/aboot-update/ /home/ecurtin/rpmbuild/SRPMS/aboot-update-0.1-2.fc36.src.rpm && cp /var/lib/mock/centos-stream+epel-9-aarch64/aboot-update/* /home/ecurtin/rpmbuild/RPMS/"
 }
 
-task2() {
+build-autosig-qemu-dtb() {
   cp ~/git/autosig-qemu-dtb/* /home/ecurtin/rpmbuild/SOURCES/
   sudo rm -rf /home/ecurtin/rpmbuild/SRPMS/autosig*.src.rpm
   rpmbuild -bs autosig-qemu-dtb.spec
   sudo podman run --rm --privileged -v /home/ecurtin/rpmbuild/:/home/ecurtin/rpmbuild/ -ti conmock /bin/bash -c "mock -a https://buildlogs.centos.org/9-stream/automotive/aarch64/packages-main/ -a https://buildlogs.centos.org/9-stream/autosd/aarch64/packages-main/ -r centos-stream+epel-9-aarch64 --rebuild --resultdir /var/lib/mock/centos-stream+epel-9-aarch64/autosig-qemu-dtb/ /home/ecurtin/rpmbuild/SRPMS/autosig-qemu-dtb-0.1-3.fc36.src.rpm && cp /var/lib/mock/centos-stream+epel-9-aarch64/autosig-qemu-dtb/* /home/ecurtin/rpmbuild/RPMS/"
 }
 
-task3() {
+build-ostree() {
   cd ~/git/ostree/
   rm -rf *.tar.xz
   rm -rf /home/ecurtin/rpmbuild/SOURCES/libostree-2022.5
@@ -33,7 +33,7 @@ task3() {
 #  sudo mock --rebuild /home/ecurtin/rpmbuild/SRPMS/ostree*.fc36.src.rpm
 }
 
-task4() {
+build-aboot-deploy() {
   cd ~/git/aboot-deploy
   cp * /home/ecurtin/rpmbuild/SOURCES/
   sudo rm -rf /home/ecurtin/rpmbuild/SRPMS/aboot-deploy*.src.rpm
@@ -54,10 +54,10 @@ sudo podman build -t conmock -f Mockfile
 
 sudo rm -rf /var/lib/mock/centos-stream+epel-9-aarch64/result
 cd /home/ecurtin/rpmbuild/SOURCES/
-task1 &
-task2 &
-task3 &
-task4 &
+build-aboot-update &
+build-autosig-qemu-dtb &
+build-ostree &
+build-aboot-deploy &
 build-osbuild-aboot &
 wait
 
