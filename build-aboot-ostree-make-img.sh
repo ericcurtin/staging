c14af5d2d2520931379814655f27087e41daeb73
Eric Curtin <ecurtin@redhat.com>
2022-10-22 15:49:51 +0100

    changes

--- build-aboot-ostree-make-img.sh
+++ build-aboot-ostree-make-img.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-set -ex
+set -x
 
 EPOCH=$(date +%s)
 ../../staging/build-aboot-ostree.sh > build-aboot-ostree$EPOCH.txt 2>&1
-sudo make cs9-abootqemu-minimal-ostree.aarch64.qcow2 > ~/abootqemu-ostree$EPOCH.txt 2>&1
+
+if [ "$RET" -eq "0" ] || [ "$RET" -eq "1" ]; then
+  set -e
+  sudo make cs9-abootqemu-minimal-ostree.aarch64.qcow2 > ~/abootqemu-ostree$EPOCH.txt 2>&1
+fi
+
+exit $?
 
