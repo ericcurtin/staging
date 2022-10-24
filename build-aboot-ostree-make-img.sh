a448e86c671a9f961a800caa9b6b0722bbb8e72b
Eric Curtin <ecurtin@redhat.com>
2022-10-24 16:11:51 +0100

    add come configs

--- build-aboot-ostree-make-img.sh
+++ build-aboot-ostree-make-img.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 EPOCH=$(date +%s)
 ../../staging/build-aboot-ostree.sh > build-aboot-ostree$EPOCH.txt 2>&1
-
-if [ "$RET" -eq "0" ] || [ "$RET" -eq "1" ]; then
-  set -e
-  sudo make cs9-abootqemu-minimal-ostree.aarch64.qcow2 > ~/abootqemu-ostree$EPOCH.txt 2>&1
-fi
-
-exit $?
+sudo make cs9-abootqemu-minimal-ostree.aarch64.qcow2 > ~/abootqemu-ostree$EPOCH.txt 2>&1
 
