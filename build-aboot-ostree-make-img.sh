8766d449c295989b0de04aea9dec92dde12a04dc
Eric Curtin <ecurtin@redhat.com>
2022-11-16 18:52:42 +0000

    nohup &

--- build-aboot-ostree-make-img.sh
+++ build-aboot-ostree-make-img.sh
@@ -5,6 +5,5 @@ set -ex
 cd ~/git/sample-images/osbuild-manifests
 
 EPOCH=$(date +%s)
-../../staging/build-aboot-ostree.sh > ~/build-aboot-ostree$EPOCH.txt 2>&1
-sudo make cs9-abootqemu-minimal-ostree.aarch64.qcow2 > ~/abootqemu-ostree$EPOCH.txt 2>&1
+nohup /bin/bash -c "../../staging/build-aboot-ostree.sh > ~/build-aboot-ostree$EPOCH.txt 2>&1 && sudo make cs9-abootqemu-minimal-ostree.aarch64.qcow2 > ~/abootqemu-ostree$EPOCH.txt 2>&1" &
 
