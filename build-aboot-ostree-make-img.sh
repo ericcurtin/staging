7147843b7e957c552767555ce99d7a968c57fbf9
Eric Curtin <ecurtin@redhat.com>
2022-11-17 14:37:22 +0000

    changes

--- build-aboot-ostree-make-img.sh
+++ build-aboot-ostree-make-img.sh
@@ -5,5 +5,7 @@ set -ex
 cd ~/git/sample-images/osbuild-manifests
 
 EPOCH=$(date +%s)
-nohup /bin/bash -c "../../staging/build-aboot-ostree.sh > ~/build-aboot-ostree$EPOCH.txt 2>&1 && sudo make cs9-abootqemu-minimal-ostree.aarch64.qcow2 > ~/abootqemu-ostree$EPOCH.txt 2>&1" &
+IMG="cs9-abootqemu-minimal-ostree"
+# IMG="cs9-abootqemu-minimal-regular"
+nohup /bin/bash -c "../../staging/build-aboot-ostree.sh > ~/build-aboot-ostree$EPOCH.txt 2>&1 && sudo make $IMG.aarch64.qcow2 > ~/$IMG$EPOCH.txt 2>&1" &
 
