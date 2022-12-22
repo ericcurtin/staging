d954034b2b4cb8a0f0d7302d0d149642ed83fd31
Eric Curtin <ecurtin@redhat.com>
2022-12-22 12:40:04 +0000

    chnages

--- build-aboot-ostree.sh
+++ build-aboot-ostree.sh
@@ -2,20 +2,25 @@
 
 set -ex
 
+USER=$(id -un)
+GID=$(id -g)
+GROUP=$(id -gn)
+usergroupadd="groupadd -g $GID $GROUP && useradd -M -s /bin/bash -g $GID -u $UID $USER"
+
 nohup sudo dnf clean all &
 
 build-aboot-update() {
   cp ~/git/aboot-update/* /home/ecurtin/rpmbuild/SOURCES/
   sudo rm -rf /home/ecurtin/rpmbuild/SRPMS/aboot-update-0.1-2.*.src.rpm
   rpmbuild -bs aboot-update.spec
-  sudo podman run --rm --privileged -v /home/ecurtin/rpmbuild/:/home/ecurtin/rpmbuild/ -ti conmock /bin/bash -c "rpmbuild -rb /home/ecurtin/rpmbuild/SRPMS/aboot-update-0.1-2.*.src.rpm && cp /var/lib/mock/centos-stream+epel-9-aarch64/aboot-update/* /home/ecurtin/rpmbuild/RPMS/"
+  sudo podman run --rm --privileged -v /home/ecurtin/rpmbuild/:/home/ecurtin/rpmbuild/ -ti conmock /bin/bash -c "$usergroupadd && rpmbuild -rb /home/ecurtin/rpmbuild/SRPMS/aboot-update-0.1-2.*.src.rpm && cp /var/lib/mock/centos-stream+epel-9-aarch64/aboot-update/* /home/ecurtin/rpmbuild/RPMS/"
 }
 
 build-autosig-qemu-dtb() {
   cp ~/git/autosig-qemu-dtb/* /home/ecurtin/rpmbuild/SOURCES/
   sudo rm -rf /home/ecurtin/rpmbuild/SRPMS/autosig*.src.rpm
   rpmbuild -bs autosig-qemu-dtb.spec
-  sudo podman run --rm --privileged -v /home/ecurtin/rpmbuild/:/home/ecurtin/rpmbuild/ -ti conmock /bin/bash -c "rpmbuild -rb /home/ecurtin/rpmbuild/SRPMS/autosig-qemu-dtb-0.1-3.*.src.rpm && cp /var/lib/mock/centos-stream+epel-9-aarch64/autosig-qemu-dtb/* /home/ecurtin/rpmbuild/RPMS/"
+  sudo podman run --rm --privileged -v /home/ecurtin/rpmbuild/:/home/ecurtin/rpmbuild/ -ti conmock /bin/bash -c "$usergroupadd && rpmbuild -rb /home/ecurtin/rpmbuild/SRPMS/autosig-qemu-dtb-0.1-3.*.src.rpm && cp /var/lib/mock/centos-stream+epel-9-aarch64/autosig-qemu-dtb/* /home/ecurtin/rpmbuild/RPMS/"
 }
 
 build-ostree() {
@@ -30,7 +35,7 @@ build-ostree() {
   tar -cJf libostree-2022.5.tar.xz libostree-2022.5
   sudo rm -rf /home/ecurtin/rpmbuild/SRPMS/ostree*.src.rpm
   rpmbuild -bs ostree.spec
-  sudo podman run --rm --privileged -v /home/ecurtin/rpmbuild/:/home/ecurtin/rpmbuild/ -ti conmock /bin/bash -c "rpmbuild -rb /home/ecurtin/rpmbuild/SRPMS/ostree*.*.src.rpm && cp /var/lib/mock/centos-stream+epel-9-aarch64/ostree/* /home/ecurtin/rpmbuild/RPMS"
+  sudo podman run --rm --privileged -v /home/ecurtin/rpmbuild/:/home/ecurtin/rpmbuild/ -ti conmock /bin/bash -c "$usergroupadd && rpmbuild -rb /home/ecurtin/rpmbuild/SRPMS/ostree*.*.src.rpm && cp /var/lib/mock/centos-stream+epel-9-aarch64/ostree/* /home/ecurtin/rpmbuild/RPMS"
 #  sudo mock --rebuild /home/ecurtin/rpmbuild/SRPMS/ostree*.fc36.src.rpm
 }
 
@@ -39,7 +44,7 @@ build-aboot-deploy() {
   cp * /home/ecurtin/rpmbuild/SOURCES/
   sudo rm -rf /home/ecurtin/rpmbuild/SRPMS/aboot-deploy*.src.rpm
   rpmbuild -bs aboot-deploy.spec
-  sudo podman run --rm --privileged -v /home/ecurtin/rpmbuild/:/home/ecurtin/rpmbuild/ -ti conmock /bin/bash -c "rpmbuild -rb /home/ecurtin/rpmbuild/SRPMS/aboot-deploy*.*src.rpm && cp /var/lib/mock/centos-stream+epel-9-aarch64/aboot-deploy/* /home/ecurtin/rpmbuild/RPMS"
+  sudo podman run --rm --privileged -v /home/ecurtin/rpmbuild/:/home/ecurtin/rpmbuild/ -ti conmock /bin/bash -c "$usergroupadd && rpmbuild -rb /home/ecurtin/rpmbuild/SRPMS/aboot-deploy*.*src.rpm && cp /var/lib/mock/centos-stream+epel-9-aarch64/aboot-deploy/* /home/ecurtin/rpmbuild/RPMS"
 }
 
 build-osbuild-aboot() {
@@ -47,7 +52,7 @@ build-osbuild-aboot() {
   cp * /home/ecurtin/rpmbuild/SOURCES/
   sudo rm -rf /home/ecurtin/rpmbuild/SRPMS/osbuild-aboot*.src.rpm
   rpmbuild -bs osbuild-aboot.spec
-  sudo podman run --rm --privileged -v /home/ecurtin/rpmbuild/:/home/ecurtin/rpmbuild/ -ti conmock /bin/bash -c "rpmbuild -rb /home/ecurtin/rpmbuild/SRPMS/osbuild-aboot*.src.rpm && cp /var/lib/mock/centos-stream+epel-9-aarch64/osbuild-aboot/* /home/ecurtin/rpmbuild/RPMS"
+  sudo podman run --rm --privileged -v /home/ecurtin/rpmbuild/:/home/ecurtin/rpmbuild/ -ti conmock /bin/bash -c "$usergroupadd && rpmbuild -rb /home/ecurtin/rpmbuild/SRPMS/osbuild-aboot*.src.rpm && cp /var/lib/mock/centos-stream+epel-9-aarch64/osbuild-aboot/* /home/ecurtin/rpmbuild/RPMS"
 }
 
 cd ~/git/staging
