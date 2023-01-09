d65c9f9e863132edd30a34b6bb2796953e1385b0
Eric Curtin <ecurtin@redhat.com>
2023-01-09 12:26:01 +0000

    build only ostree

--- build-aboot-ostree-make-img.sh
+++ build-aboot-ostree-make-img.sh
@@ -13,7 +13,8 @@ EPOCH=$(date +%s)
 # type_img="abootqemu-minimal-regular"
 uname_m=$(uname -m)
 
-for type_img in abootqemu-minimal-ostree abootqemu-minimal-regular; do
+#for type_img in abootqemu-minimal-ostree abootqemu-minimal-regular; do
+for type_img in abootqemu-minimal-ostree; do
 img="cs9-$type_img.$uname_m.qcow2"
 
 # sudo make cs9-abootqemu-minimal-ostree.aarch64.qcow2 > ~/abootqemu-ostree$EPOCH.txt 2>&1
