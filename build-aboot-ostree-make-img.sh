b143164f2f756e5a72b2376fa6c22eca8718c198
Eric Curtin <ecurtin@redhat.com>
2023-01-11 18:08:52 +0000

    build 4 images in parallel

--- build-aboot-ostree-make-img.sh
+++ build-aboot-ostree-make-img.sh
@@ -13,13 +13,15 @@ EPOCH=$(date +%s)
 # type_img="abootqemu-minimal-regular"
 uname_m=$(uname -m)
 
-for type_img in abootqemu-minimal-ostree abootqemu-minimal-regular; do
+for type_img in abootqemu-minimal-ostree abootqemu-minimal-regular qemu-minimal-ostree qemu-minimal-regular; do
 #for type_img in abootqemu-minimal-ostree; do
 img="cs9-$type_img.$uname_m.qcow2"
 
 # sudo make cs9-abootqemu-minimal-ostree.aarch64.qcow2 > ~/abootqemu-ostree$EPOCH.txt 2>&1
 # sudo make $img DEFINES='extra_repos=[{"id":"extra","baseurl":"file:///home/ecurtin/rpmbuild/RPMS/"}]' > ~/$type_img$EPOCH.txt 2>&1
-sudo make $img > ~/$type_img$EPOCH.txt 2>&1
+/bin/bash -c "sudo make $img > ~/$type_img$EPOCH.txt 2>&1" &
 sudo chown $un:$un $img
 done
+
+wait
 
