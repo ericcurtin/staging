43dca767bb8d5d57b9fdde9292807aa863ad765b
Eric Curtin <ecurtin@redhat.com>
2023-02-28 13:45:52 +0000

    test upgrade

--- build-aboot-ostree-make-img.sh
+++ build-aboot-ostree-make-img.sh
@@ -19,11 +19,12 @@ img="cs9-$type_img.$uname_m.qcow2"
 
 if [[ "$type_img" == *-ostree ]]; then
   ostree_repo="OSTREE_REPO=ostree-repo"
+  img_repo="cs9-$type_img.$uname_m.repo"
 fi
 
 # sudo make cs9-abootqemu-minimal-ostree.aarch64.qcow2 > ~/abootqemu-ostree$EPOCH.txt 2>&1
 # sudo make $img DEFINES='extra_repos=[{"id":"extra","baseurl":"file:///home/ecurtin/rpmbuild/RPMS/"}]' > ~/$type_img$EPOCH.txt 2>&1
-/bin/bash -c "sudo make $img $ostree_repo > ~/$type_img$EPOCH.txt 2>&1" &
+/bin/bash -c "sudo make $img $ostree_repo > ~/$type_img$EPOCH.txt 2>&1 && sudo make $img_repo DEFINES='extra_rpms=[\"git\"] distro_version=\"9.1\"' OSTREE_REPO=ostree-repo" &
 done
 
 wait
