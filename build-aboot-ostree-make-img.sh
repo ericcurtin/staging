c3238a10a66d9dc198135bae9e0ec245f123b68f
Eric Curtin <ecurtin@redhat.com>
2023-02-28 15:58:27 +0000

    changes

--- build-aboot-ostree-make-img.sh
+++ build-aboot-ostree-make-img.sh
@@ -13,18 +13,26 @@ EPOCH=$(date +%s)
 # type_img="abootqemu-minimal-regular"
 uname_m=$(uname -m)
 
+makes() {
+  sudo make $img $ostree_repo > ~/$type_img$EPOCH.txt 2>&1
+  if [ -n "$img_repo" ]; then
+     sudo make $img_repo DEFINES='extra_rpms=[\"git\"] distro_version=\"9.1\"' OSTREE_REPO=ostree-repo
+  fi
+}
+
 for type_img in abootqemu-minimal-ostree abootqemu-minimal-regular qemu-minimal-ostree qemu-minimal-regular; do
 #for type_img in abootqemu-minimal-ostree; do
-img="cs9-$type_img.$uname_m.qcow2"
+  img="cs9-$type_img.$uname_m.qcow2"
 
-if [[ "$type_img" == *-ostree ]]; then
-  ostree_repo="OSTREE_REPO=ostree-repo"
-  img_repo="cs9-$type_img.$uname_m.repo"
-fi
+  if [[ "$type_img" == *-ostree ]]; then
+    ostree_repo="OSTREE_REPO=ostree-repo"
+    img_repo="cs9-$type_img.$uname_m.repo"
+  fi
 
+  makes &
 # sudo make cs9-abootqemu-minimal-ostree.aarch64.qcow2 > ~/abootqemu-ostree$EPOCH.txt 2>&1
 # sudo make $img DEFINES='extra_repos=[{"id":"extra","baseurl":"file:///home/ecurtin/rpmbuild/RPMS/"}]' > ~/$type_img$EPOCH.txt 2>&1
-/bin/bash -c "sudo make $img $ostree_repo > ~/$type_img$EPOCH.txt 2>&1 && sudo make $img_repo DEFINES='extra_rpms=[\"git\"] distro_version=\"9.1\"' OSTREE_REPO=ostree-repo" &
+#  /bin/bash -c "sudo make $img $ostree_repo > ~/$type_img$EPOCH.txt 2>&1 && sudo make $img_repo DEFINES='extra_rpms=[\"git\"] distro_version=\"9.1\"' OSTREE_REPO=ostree-repo" &
 done
 
 wait
