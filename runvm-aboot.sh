da65d3715ecc9d43563e42b3df08d25b1a4b50e8
Eric Curtin <ecurtin@redhat.com>
2023-02-28 17:33:19 +0000

    changes

--- runvm-aboot.sh
+++ runvm-aboot.sh
@@ -4,5 +4,5 @@ set -ex
 
 cd ~/git/sample-images/osbuild-manifests
 
-taskset -c 4-7 ./runvm --publish-dir=cs9-$1-repo --aboot --nographics cs9-$1.aarch64.qcow2 -smp 4
+taskset -c 4-7 ./runvm --publish-dir=$1-repo --aboot --nographics cs9-$1.aarch64.qcow2 -smp 4
 
