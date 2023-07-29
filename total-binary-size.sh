3100d32d1c090b1997da8f9224a89cc942f57ba0
Eric Curtin <ecurtin@redhat.com>
2023-07-29 07:34:05 +0100

    Use hc

--- total-binary-size.sh
+++ total-binary-size.sh
@@ -2,13 +2,11 @@
 
 bin=$(command -v $1)
 
-du -sh $bin
-
 for i in $(ldd $bin); do
-   out=$(du -sh $i 2> /dev/null)
-   if [ $? -eq 0 ]; then
-     echo "  $out"
+   if [ -e "$i" ]; then
+     bin="$bin $i"
    fi
 done
 
+du -hc $bin
 
