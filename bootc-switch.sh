fa451d5c0b2db6f6df3166bc10df24065657bb15
Eric Curtin <ecurtin@redhat.com>
2025-01-06 14:54:25 +0000

    Script only runs as root
    Signed-off-by: Eric Curtin <ecurtin@redhat.com>

--- bootc-switch.sh
+++ bootc-switch.sh
@@ -2,6 +2,12 @@
 
 main() {
   set -exu -o pipefail
+
+  if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    return 1
+  fi
+
   podman build -t bootc -f Containerfile-bootc
   bootc switch --transport containers-storage bootc
 }
