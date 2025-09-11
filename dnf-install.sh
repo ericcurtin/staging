44547a3a08daa8e388f057635ae36c23018f7f47
Eric Curtin <eric.curtin@docker.com>
2025-09-11 20:47:24 +0100

    Only install rocm stuff on x86_64
    Signed-off-by: Eric Curtin <eric.curtin@docker.com>

--- dnf-install.sh
+++ dnf-install.sh
@@ -9,12 +9,11 @@ main() {
     fedora-workstation-repositories gcc hyperfine keepassxc libcurl-devel \
     make nvtop python3-tqdm qemu-kvm the_silver_searcher vim python3-flake8 \
     bats httpd-tools ninja meson python3-jinja2 SDL2-devel crun-krun podman \
-    shellcheck git-clang-format rocm-core-devel hipblas-devel rocblas-devel \
-    rocm-hip-devel vulkan-headers vulkan-loader-devel glslang glslc \
-    spirv-tools
+    shellcheck git-clang-format vulkan-loader-devel glslang glslc spirv-tools
 
   if [ $(uname -m) != "aarch64" ]; then
-    dnf install -y --enablerepo="google-chrome" wine google-chrome-stable
+    dnf install -y --enablerepo="google-chrome" wine google-chrome-stable \
+      rocm-core-devel hipblas-devel rocblas-devel rocm-hip-devel
   fi
 
   dnf remove -y nano
