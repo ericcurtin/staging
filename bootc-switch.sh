7ace2157061ea151868003264b21195c1ac5c869
Eric Curtin <eric.curtin@docker.com>
2025-10-25 21:57:05 +0100

    docker
    Signed-off-by: Eric Curtin <eric.curtin@docker.com>

--- bootc-switch.sh
+++ bootc-switch.sh
@@ -2,20 +2,20 @@
 
 ls_images() {
   local sudo=$1
-  $sudo podman image ls -a
+  $sudo $conman image ls -a
 }
 
 rm_images() {
   local sudo=$1
-  xargs -r "$sudo" podman rmi -f > /dev/null 2>&1
+  xargs -r "$sudo" $conman rmi -f > /dev/null 2>&1
 }
 
 filter_for_none() {
   grep '<none>' | awk '{print $3}'
 }
 
-podman_load() {
-  podman -c podman-machine-default-root load
+conman_load() {
+  $conman -c podman-machine-default-root load
 }
 
 main() {
@@ -26,11 +26,13 @@ main() {
     return 1
   fi
 
+  local conman="docker"
+
   # --no-cache, --network host fixes bug
   if sudo bootc status | grep -q machine-os; then
-    sudo podman build -t bootc -f Containerfile-bootc-podman-machine
+    sudo $conman build -t bootc -f Containerfile-bootc-$conman-machine
   else
-    sudo podman build -t bootc -f Containerfile-bootc
+    sudo $conman build -t bootc -f Containerfile-bootc
   fi
 
   ls_images "sudo" | filter_for_none | rm_images "sudo" &
@@ -39,12 +41,12 @@ main() {
   sudo chcon --reference /usr/bin/rpm-ostree /usr/bin/bootc
   local image_name="localhost/bootc"
   local id
-  id="$(sudo podman images -q $image_name)"
-  # sudo podman save localhost/bootc | podman_load > /dev/null 2>&1 &
-  # sudo podman save "$id" | podman load > /dev/null 2>&1 &
+  id="$(sudo $conman images -q $image_name)"
+  # sudo $conman save localhost/bootc | $conman_load > /dev/null 2>&1 &
+  # sudo $conman save "$id" | $conman load > /dev/null 2>&1 &
   sudo bootc switch --transport containers-storage "$id"
   wait
-  # podman tag "$id" "$image_name"
+  # $conman tag "$id" "$image_name"
 }
 
 main "$@"
